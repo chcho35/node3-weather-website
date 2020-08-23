6865ebfd05aad20279f85505d4407bb37fb254c6
Christine <chcho35@student.monash.edu>
2020-08-23 17:10:23 +1000

    Add more data to weather forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,14 +3,15 @@ const request = require('postman-request')
 //json:true will return a a json body object, so we don't have to parse it
 const forecast = (latitude, longitude, callback) =>{
     const weatherUrl = `http://api.weatherstack.com/current?access_key=a2e19c76fca5ccfe651e350ec755656e&query=${latitude},${longitude}`
-    request({url: weatherUrl, json: true}, (error, {body}) => {
+    request({url: weatherUrl, json: true}, (error, {body} = {}) => {
         if(error){
             callback('Unable to connect to network', undefined)
         } else if(body.error){
             callback(body.error.info, undefined)
         }
         else{
-            callback(undefined,`${body.current.weather_descriptions[0]}. Temperature is ${body.current.temperature} degrees, feels like ${body.current.feelslike} degrees`)
+            callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees, feels like ${body.current.feelslike} degrees.
+            The humidity is ${body.current.humidity} and UV Index is ${body.current.uv_index}.`)
         }
     })
 }
